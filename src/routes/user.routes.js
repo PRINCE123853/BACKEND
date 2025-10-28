3974ba259bb30acdf162a7e749e29a473cad0b0a
PRINCE123853 <princekumar2005itaha@gmail.com>
2025-10-29 00:45:42 +0530

    Add new routes for user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,23 @@
 
 // route means like    ./  , ./profaile , ./profile/temp 
+
 import {Router} from "express"; // router comes from express
-import { loginUser, logoutUser, registerUser, refreshAccessToken } from "../controllers/user.controller.js";
+import { 
+  loginUser, 
+  logoutUser, 
+  registerUser, 
+  refreshAccessToken, 
+  changeCurrentPassword, 
+  getCurrentUser, 
+  updateAccountDetails, // vdo X
+  updateUserAvatar, 
+  updateUserCoverImage, 
+  getUserChannelProfile, 
+  getWatchHistory 
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js"; 
+// import { JsonWebTokenError } from "jsonwebtoken";
 
 
 
@@ -30,6 +44,19 @@ router.route("/login").post(loginUser)
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)// yaha user ko direct data chahiye koi set nhi kar raha hai isiliye get.
+router.route("/update-account").patch(verifyJWT,updateAccountDetails )// patch rakhte hai nhi to sari detail update ho jayegi, patch me particular hoga
+
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-Image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+
+
+//yaha params se data aa raha hai jab url se aata hai to ye format sahi rahega. "c->channel" anythinhg
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+
+router.route("/history").get(verifyJWT, getWatchHistory)
+
 
 export default router // dafter defalut we can import anyware as own wish name . if not default then same name
 
